05828547a52cdf9bdd3495b7716977f00794b9b8
NikshitaVasu <nikshitavit121@siesgst.ac.in>
2024-09-26 21:33:43 +0530

    Added various features like search , sortimg , dark mode , add pdf as well as view pdf

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,73 +1,3 @@
-
-import React, { useState, useEffect } from 'react';
-import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
-import logo from '../../assets/logo.svg';
-import './navbar.css';
-
-const Menu = () => (
-  <>
-    <p>
-      <a href='/'>Home</a>
-    </p>
-    <p>
-      <a href='/books'>View Books</a>
-    </p>
-    <p>
-      <a href='/addBook'>Add Book</a>
-    </p>
-  </>
-);
-
-const Navbar = () => {
-  const [toggleMenu, setToggleMenu] = useState(false);
-  const [darkMode, setDarkMode] = useState(false);
-
-  useEffect(() => {
-    const savedMode = localStorage.getItem('darkMode') === 'true';
-    setDarkMode(savedMode);
-    document.body.classList.toggle('dark-mode', savedMode);
-  }, []);
-
-  const toggleDarkMode = () => {
-    const newDarkMode = !darkMode;
-    setDarkMode(newDarkMode);
-    localStorage.setItem('darkMode', newDarkMode);
-    document.body.classList.toggle('dark-mode', newDarkMode);
-  };
-
-  return (
-    <nav className='navbar'>
-      <div className='navbar-links'>
-        <div className='navbar-links_logo'>
-          <img src={logo} alt='logo' />
-        </div>
-        <div className='navbar-links_container'>
-          <Menu />
-        </div>
-      </div>
-      <div className='navbar-menu'>
-        {toggleMenu ? (
-          <RiCloseLine color='#000' size={27} onClick={() => setToggleMenu(false)} />
-        ) : (
-          <RiMenu3Line color='#000' size={27} onClick={() => setToggleMenu(true)} />
-        )}
-        {toggleMenu && (
-          <div className='navbar-menu_container scale-up-center'>
-            <div className='navbar-menu_container-links'>
-              <Menu />
-            </div>
-          </div>
-        )}
-      </div>
-      <button className='dark-mode-toggle' onClick={toggleDarkMode}>
-        {darkMode ? 'Light Mode' : 'Dark Mode'}
-      </button>
-    </nav>
-  );
-};
-
-export default Navbar;
-=======
 import React, { useState } from 'react';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../assets/logo.svg';
@@ -118,4 +48,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
->>>>>>> a16c129e84ced801d680ab3b52a17d3d3bc7123e
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import hero from '../../assets/hero.jpg';
 import './home.css';
@@ -21,27 +20,3 @@ const Home = () => {
 };
 
 export default Home;
-=======
-import React from 'react';
-import hero from '../../assets/hero.jpg';
-import './home.css';
-import 'animate.css';
-
-const Home = () => {
-  return (
-    <section className='home-page'>
-      <div className='hero-container animate__animated animate__backInDown'>
-        <h1 className='hero__title'>Welcome to BXTrack Bookstore!</h1>
-        <img src={hero} alt='Family reading books' className='hero__image' />
-      </div>
-      <div className='home-page__btn animate__animated animate__backInDown'>
-        <a href='/books'>
-          <button>View Collection</button>
-        </a>
-      </div>
-    </section>
-  );
-};
-
-export default Home;
->>>>>>> a16c129e84ced801d680ab3b52a17d3d3bc7123e
